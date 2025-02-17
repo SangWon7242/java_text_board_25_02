a5254f00d14a8b5aaa38817df08e420c024e2074
SangWon7242 <gotkdqja@gamil.com>
2025-02-17 17:22:24 +0900

    명령어 입력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,9 +1,19 @@
 package com.sbs.java.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("== 자바 텍스트 게시판 시작 ==");
-    System.out.println("텍스트 게시판을 시작합니다.");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("명령) ");
+    String cmd = sc.nextLine();
+
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
+
     System.out.println("== 자바 텍스트 게시판 끝 ==");
+
+    sc.close();
   }
 }
