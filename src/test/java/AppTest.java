3768ae49e13e811f1ddfc7bd05451c4b92c3e60c
SangWon7242 <gotkdqja@gamil.com>
2025-02-19 16:43:03 +0900

    List 두 개를 이용하여 쿼리스트링을 다룰 때 발생하는 문제점

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,28 +5,35 @@ import java.util.List;
 public class AppTest {
   public static void main(String[] args) {
     // 파라미터 분석
-    String queryString = "d=4&a=1&b=2&c=3&f=6";
+    String queryString = "id=1&subject=제목1&content=내용1&writerName=김철수&boardId=1";
 
     String[] queryStringBits = queryString.split("&");
 
     System.out.println(Arrays.toString(queryStringBits));
 
     List<String> paramsName = new ArrayList<>();
-    List<Integer> paramsValue = new ArrayList<>();
+    List<String> paramsValue = new ArrayList<>();
 
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
 
       paramsName.add(bitBits[0]);
-      paramsValue.add(Integer.parseInt(bitBits[1]));
+      paramsValue.add(bitBits[1]);
     }
 
+    /*
     for(int i = 0; i < paramsName.size(); i++) {
       String paramName = paramsName.get(i);
-      int paramValue = paramsValue.get(i);
+      String paramValue = paramsValue.get(i);
 
-      System.out.printf("%s : %d\n", paramName, paramValue);
+      System.out.printf("%s : %s\n", paramName, paramValue);
     }
+    */
+    System.out.println(paramsName);
+    System.out.println(paramsValue);
 
+    int findIndex = paramsName.indexOf("subject");
+    System.out.println(paramsValue.get(findIndex));
+    
   }
 }
