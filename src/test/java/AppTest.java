dbdd746564c56b0ab64876ef90dd2fadf3dc925b
SangWon7242 <gotkdqja@gamil.com>
2025-02-19 17:13:44 +0900

    Util::getParams 도입으로 쿼리스트링 분석을 쉽게 하도록

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,33 +4,30 @@ public class AppTest {
   public static void main(String[] args) {
     // 파라미터 분석
     String queryString1 = "id=1&subject=제목1&content=내용1&writerName=김철수&boardId=1";
-    String[] queryString1Bits = queryString1.split("&");
+    Map<String, String> params1 = Util.getParams(queryString1);
+
+    System.out.println(params1);
 
     String queryString2 = "id=2&subject=제목2&content=내용2&writerName=신유리&boardId=2";
-    String[] queryString2Bits = queryString2.split("&");
+    Map<String, String> params2 = Util.getParams(queryString2);
 
-    // Map<String, String> params = new HashMap<>();
-    Map<String, String> params1 = new LinkedHashMap<>();
+    System.out.println(params2);
 
-    for(String bit : queryString1Bits) {
+  }
+}
+
+class Util {
+  static Map<String, String> getParams(String queryStr) {
+    Map<String, String> params = new LinkedHashMap<>();
+
+    String[] queryStrBits = queryStr.split("&");
+
+    for(String bit : queryStrBits) {
       String[] bitBits = bit.split("=");
 
-      params1.put(bitBits[0], bitBits[1]);
+      params.put(bitBits[0], bitBits[1]);
     }
 
-    // Map<String, String> params = new HashMap<>();
-    Map<String, String> params2 = new LinkedHashMap<>();
-
-    for(String bit : queryString2Bits) {
-      String[] bitBits = bit.split("=");
-
-      params2.put(bitBits[0], bitBits[1]);
-    }
-
-    System.out.println("== 반복문을 이용한 순회 ==");
-    params1.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
-
-    System.out.println("== 반복문을 이용한 순회 ==");
-    params2.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
+    return params;
   }
 }
