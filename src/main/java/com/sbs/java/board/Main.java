398eeb3ebf5f8ce7b38be9f392dc5225b92c59d6
SangWon7242 <gotkdqja@gamil.com>
2025-02-20 17:17:48 +0900

    id 값에 대한 유효성 처리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -62,7 +62,20 @@ public class Main {
         }
 
         Map<String, String> params = rq.getParams();
-        int id = Integer.parseInt(params.get("id"));
+
+        if(!params.containsKey("id")) {
+          System.out.println("id 값을 입력해주세요.");
+          continue;
+        }
+
+        int id = 0;
+
+        try {
+          id = Integer.parseInt(params.get("id"));
+        } catch (NumberFormatException e) {
+          System.out.println("id를 정수형태로 입력해주세요.");
+          continue;
+        }
 
         if(id > articles.size()) {
           System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
