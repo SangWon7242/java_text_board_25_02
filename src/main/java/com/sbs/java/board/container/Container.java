226076a1f84ad4af346213035e9c7aabfffb557e
SangWon7242 <gotkdqja@gamil.com>
2025-02-24 17:26:03 +0900

    articleController 도입

--- src/main/java/com/sbs/java/board/container/Container.java
+++ src/main/java/com/sbs/java/board/container/Container.java
@@ -1,12 +1,18 @@
 package com.sbs.java.board.container;
 
+import com.sbs.java.board.boundedContext.article.controller.ArticleController;
+
 import java.util.Scanner;
 
 public class Container {
   public static Scanner sc;
 
+  public static ArticleController articleController;
+
   // static 메서드는 프로그램 실행되자마자 실행
   static {
     sc = new Scanner(System.in);
+
+    articleController = new ArticleController();
   }
 }
