1bb705f9bc4423ee1cc50574979cda7a6d3fcc09
SangWon7242 <gotkdqja@gamil.com>
2025-02-25 17:10:52 +0900

    롬복 적용하여 소스코드 다이어트

--- src/main/java/com/sbs/java/board/base/Rq.java
+++ src/main/java/com/sbs/java/board/base/Rq.java
@@ -1,26 +1,22 @@
 package com.sbs.java.board.base;
 
 import com.sbs.java.board.util.Util;
+import lombok.Getter;
 
 import java.util.Map;
 
 public class Rq {
-  public String url;
-  public Map<String, String> params;
-  public String urlPath;
+  private String url;
+
+  @Getter
+  private Map<String, String> params;
+  @Getter
+  private String urlPath;
 
   public Rq(String url) {
     this.url = url;
     params = Util.getParamsFromUrl(this.url);
     urlPath = Util.getPathFromUrl(this.url);
-  }
-
-  public Map<String, String> getParams() {
-    return params;
-  }
-
-  public String getUrlPath() {
-    return urlPath;
   }
 
   public int getIntParam(String paramName, int defaultValue) {
