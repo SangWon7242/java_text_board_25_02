c56e02d880e41579c2921618bedd72bf053bdb50
SangWon7242 <gotkdqja@gamil.com>
2025-02-26 16:46:39 +0900

    회원 테스트 데이터 구현

--- src/main/java/com/sbs/java/board/boundedContext/member/service/MemberService.java
+++ src/main/java/com/sbs/java/board/boundedContext/member/service/MemberService.java
@@ -4,11 +4,21 @@ import com.sbs.java.board.boundedContext.member.dto.Member;
 import com.sbs.java.board.boundedContext.member.repository.MemberRepository;
 import com.sbs.java.board.container.Container;
 
+import java.util.stream.IntStream;
+
 public class MemberService {
   private MemberRepository memberRepository;
 
   public MemberService() {
     memberRepository = Container.memberRepository;
+
+    makeMemberTestData();
+  }
+
+  public void makeMemberTestData() {
+    join("user1", "1234", "홍길동");
+    join("love", "12345", "김철수");
+    join("java", "1111", "제임스");
   }
 
   public void join(String username, String password, String name) {
